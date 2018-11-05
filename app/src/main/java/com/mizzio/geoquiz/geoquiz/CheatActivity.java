a00c2af39af892fd810932a3bad24672c05f6616
Mionekos <maxrai020797>
2018-11-05 16:52:47 +0500

    Save answer in CheatActivity when user rotate phone; Add layout-land.

--- app/src/main/java/com/mizzio/geoquiz/geoquiz/CheatActivity.java
+++ app/src/main/java/com/mizzio/geoquiz/geoquiz/CheatActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +13,8 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "com.mizzio.geoquiz.qeoquiz.answer_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.mizzio.geoquiz.qeoquiz.answer_shown";
+    private static final String TAG = "CheatActivity";
+    private static final String TEXT_ANSWER = "text_answer";
 
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
@@ -26,13 +29,25 @@ public class CheatActivity extends AppCompatActivity {
     public static  boolean wasAnswerShown(Intent result){
         return result.getBooleanExtra(EXTRA_ANSWER_SHOWN,false);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE,false);
 
         mAnswerTextView = findViewById(R.id.answer_text_view);
+        String text = "";
+
+        if (savedInstanceState!=null){
+            text = savedInstanceState.getString(TEXT_ANSWER);
+        }
+        mAnswerTextView.setText(text);
+
+
+
+
         mShowAnswerButton = findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +68,41 @@ public class CheatActivity extends AppCompatActivity {
         Intent data = new Intent();
         data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
         setResult(RESULT_OK,data);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+        TextView textAnswer = findViewById(R.id.answer_text_view);
+        saveInstanceState.putString(TEXT_ANSWER,textAnswer.getText().toString());
+        Log.i(TAG,"onSaveInstanceState");
+    }
+
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
     }
 }
